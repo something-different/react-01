8c7390b15c4d8f31ed353237afd2d2ca2052f739
something-different <lisiyu9610@mail.nwpu.edu.cn>
2020-06-21 22:34:11 +0800

    2020-6-21

--- src/05.class继承.js
+++ src/05.class继承.js
@@ -9,9 +9,27 @@ class Person{
 }
 //在class类中可以使用extends进行继承
 //语法class 子类 extends 父类
-class Chinese extends Person{
-    
+class American extends Person{
+    constructor(name ,age){
+        //问题一：为什么要调用super
+        //       答：子类通过extends继承了父类，那么在子类中的constructor必须优先调用super
+        //问题二：super是个什么东西
+        //       答：super是一个函数，而且是父类的构造器，子类的super就是父类中的constructor的引用
+        //super()//undefined
+        //问题三：为什么调用super之后，a1实例中的name和age都变成undefined了
+        //       答：super是父类的构造器，此时并没有赋值，所以是undefined
+        super(name,age)//传参
+    }
 }
-const c1=new Chinese('jack',12)
+const a1=new American('mary',15)
+console.log(a1)
+console.log(a1.sayHello());
+class Chinese extends Person{
+    constructor(name,age,idNumber){
+        super(name,age)
+        this.idNumber=idNumber//注意：this不能出现在super之前
+    }
+}
+const c1=new Chinese('jack',12,'1234567')
 console.log(c1);
 console.log(c1.sayHello());
